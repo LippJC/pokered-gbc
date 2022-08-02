144b03428244316ae986ff5ada65542ef5268997
LippJC <lipp.jessec@gmail.com>
2022-08-02 19:13:55 -0400

    Update Route4.asm

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -3,22 +3,12 @@ Route4Mons:
 	db 10, RATTATA
 	db 10, SPEAROW
 	db  8, RATTATA
-IF DEF(_RED)
-	db  6, EKANS
-	db  8, SPEAROW
-	db 10, EKANS
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, EKANS
-	db 12, EKANS
-ENDC
-IF DEF(_BLUE)
-	db  6, SANDSHREW
-	db  8, SPEAROW
-	db 10, SANDSHREW
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, SANDSHREW
-	db 12, SANDSHREW
-ENDC
+	db  9, EKANS
+	db  9, SANDSHREW
+	db 10, SPEAROW   ; 29.7
+	db 12, EKANS     ; 14.9
+	db 12, SANDSHREW ; 14.9
+	db 10, JIGGLYPUFF;  4.3
+	db 14, RATTATA   ; 36.3
+
 	db 0 ; water encounter rate
