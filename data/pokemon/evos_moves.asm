3abe2e3c90edeb9906611537d81dd215b7c74549
LippJC <lipp.jessec@gmail.com>
2022-08-01 22:13:01 -0400

    Update evos_moves.asm
    JIGGLYPUFF LINE

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -1266,6 +1266,13 @@ WigglytuffEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
+	db 9, POUND
+	db 14, DISABLE
+	db 19, DEFENSE_CURL
+	db 24, DOUBLESLAP
+	db 29, REST
+	db 34, BODY_SLAM
+	db 39, DOUBLE_EDGE
 	db 0
 
 EeveeEvosMoves:
