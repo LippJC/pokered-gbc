bebe0f65d7e23828915259320695350a43c57f95
LippJC <lipp.jessec@gmail.com>
2022-08-01 22:10:23 -0400

    Update evos_moves.asm
    CLEFAIRY LINE

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -1664,6 +1664,12 @@ ClefableEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
+	db 13, SING
+	db 18, DOUBLESLAP
+	db 24, MINIMIZE
+	db 31, METRONOME
+	db 39, DEFENSE_CURL
+	db 48, LIGHT_SCREEN
 	db 0
 
 WeezingEvosMoves:
