0b2bf118c1ba6ed52a87106b7f86ab73150a7d16
LippJC <lipp.jessec@gmail.com>
2022-08-02 18:28:08 -0400

    Update MtMoon1F.asm

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,14 +1,14 @@
 MoonMons1:
 	db 10 ; grass encounter rate
-	db  8, ZUBAT
 	db  7, ZUBAT
+	db  6, ZUBAT
 	db  9, ZUBAT
 	db  8, GEODUDE
-	db  6, ZUBAT
+	db  9, GEODUDE ; 19.6
+	db  8, PARAS   ;  9.8
 	db 10, ZUBAT
-	db 10, GEODUDE
-	db  8, PARAS
 	db 11, ZUBAT
-	db  8, CLEFAIRY
+	db 12, ZUBAT   ; 69.5
+	db  8, CLEFAIRY ; 1.2
 
 	db 0 ; water encounter rate
