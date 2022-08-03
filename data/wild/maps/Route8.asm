f90a3696993b6728d98d302df0154cb917ed2957
LippJC <lipp.jessec@gmail.com>
2022-08-03 08:31:59 -0400

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -1,27 +1,14 @@
 Route8Mons:
 	db 15 ; grass encounter rate
-	db 18, PIDGEY
-IF DEF(_RED)
-	db 18, MANKEY
-	db 17, EKANS
-	db 16, GROWLITHE
-	db 20, PIDGEY
-	db 20, MANKEY
-	db 19, EKANS
-	db 17, GROWLITHE
-	db 15, GROWLITHE
-	db 18, GROWLITHE
-ENDC
-IF DEF(_BLUE)
-	db 18, MEOWTH
-	db 17, SANDSHREW
-	db 16, VULPIX
-	db 20, PIDGEY
-	db 20, MEOWTH
-	db 19, SANDSHREW
-	db 17, VULPIX
-	db 15, VULPIX
+	db 20, PIDGEY    ; 19.9
+	db 20, RATTATA   ; 19.9
+	db 19, JIGGLYPUFF; 15.2
 	db 18, VULPIX
-ENDC
+	db 18, GROWLITHE
+	db 19, ABRA	     ;  9.8
+	db 20, VULPIX	 ; 14.9
+	db 20, GROWLITHE ; 14.9
+	db 19, MANKEY	 ;  4.3
+	db 20, KADABRA   ;  1.2
 
 	db 0 ; water encounter rate
