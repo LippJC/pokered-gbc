66bedd5b0a7b87a9b79549e01aa5afebf9832f6f
LippJC <lipp.jessec@gmail.com>
2022-08-02 21:11:16 -0400

    Update Route5.asm

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -1,14 +1,14 @@
 Route5Mons:
 	db 15 ; grass encounter rate
-	db 13, ODDISH    ; 19.9
-	db 13, BELLSPROUT; 19.9
+	db 13, ODDISH    
+	db 13, BELLSPROUT
 	db  7, JIGGLYPUFF; 15.2
 	db 11, MANKEY
 	db 11, MEOWTH
-	db 16, MANKEY
-	db 15, MANKEY    ; 24.7
-	db 16, MEOWTH
-	db 15, MEOWTH	 ; 19.2
+	db 16, MANKEY    ; 19.6
+	db 15, ODDISH    ; 24.7
+	db 16, BELLSPROUT; 24.7
+	db 15, MEOWTH	 ; 14.1
 	db 17, GLOOM	 ;  1.2
 
 	db 0 ; water encounter rate
