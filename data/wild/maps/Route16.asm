3e7e7d7769ded07d198c3cf523e4adef0a1be3cd
LippJC <lipp.jessec@gmail.com>
2022-08-03 11:01:28 -0400

    Update Route16.asm

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,14 +1,14 @@
 Route16Mons:
 	db 25 ; grass encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 18, RATTATA
-	db 20, DODUO
-	db 20, RATTATA
-	db 18, DODUO
-	db 22, DODUO
-	db 22, RATTATA
-	db 23, RATICATE
-	db 25, RATICATE
+	db 22, VENONAT   ; 19.9
+	db 23, DODUO
+	db 25, SPEAROW   ; 15.2
+	db 24, DODUO
+	db 24, SANDSHREW ;  9.8
+	db 27, DODUO	 ; 39.5
+	db 24, BUTTERFREE;  5.1
+	db 24, VENOMOTH  ;  5.1
+	db 26, FEAROW	 ;  4.3
+	db 30, ARBOK	 ;	1.2
 
 	db 0 ; water encounter rate
