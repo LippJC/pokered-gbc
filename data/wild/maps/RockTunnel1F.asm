901ad06691eb28a924eba4da907371a91b711874
LippJC <lipp.jessec@gmail.com>
2022-08-02 22:44:27 -0400

    Update RockTunnel1F.asm

--- data/wild/maps/RockTunnel1F.asm
+++ data/wild/maps/RockTunnel1F.asm
@@ -1,14 +1,14 @@
 TunnelMonsB1:
 	db 15 ; grass encounter rate
-	db 16, ZUBAT
-	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
 	db 15, ZUBAT
-	db 17, MACHOP
-	db 13, ONIX
+	db 16, ZUBAT
+	db 17, GEODUDE
+	db 18, MACHOP
+	db 20, GEODUDE
+	db 18, ZUBAT
+	db 17, ZUBAT
+	db 19, MACHOP
 	db 15, ONIX
+	db 21, ONIX
 
 	db 0 ; water encounter rate
