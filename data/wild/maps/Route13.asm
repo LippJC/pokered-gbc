ef0ca22b7aa489495f5b9416f28dfd8461af85f2
LippJC <lipp.jessec@gmail.com>
2022-08-03 09:07:04 -0400

    Update Route13.asm

--- data/wild/maps/Route13.asm
+++ data/wild/maps/Route13.asm
@@ -1,28 +1,14 @@
 Route13Mons:
 	db 20 ; grass encounter rate
-IF DEF(_RED)
-	db 24, ODDISH
+	db 24, VENONAT	 ; 19.9
 	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
+	db 27, PIDGEY	 ; 35.1
+	db 26, BELLSPROUT;  9.8
+	db 26, ODDISH	 ;  9.8
+	db 26, PIGEOTTO	 ;  9.8
+	db 28, GLOOM	 ;  5.1
+	db 28, WEEPINBELL;  5.1
 	db 25, DITTO
-	db 28, GLOOM
-	db 30, GLOOM
-ENDC
-IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 25, DITTO
-	db 28, WEEPINBELL
-	db 30, WEEPINBELL
-ENDC
+	db 30, DITTO	 ;  5.5
 
 	db 0 ; water encounter rate
