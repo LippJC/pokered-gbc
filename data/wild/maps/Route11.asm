198026d372fd1b48cb10b86b6ba10b880fd1609b
LippJC <lipp.jessec@gmail.com>
2022-08-02 21:46:26 -0400

    Update Route11.asm

--- data/wild/maps/Route11.asm
+++ data/wild/maps/Route11.asm
@@ -1,25 +1,14 @@
 Route11Mons:
 	db 15 ; grass encounter rate
-IF DEF(_RED)
 	db 14, EKANS
-	db 15, SPEAROW
-	db 12, EKANS
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
-	db 15, EKANS
-ENDC
-IF DEF(_BLUE)
 	db 14, SANDSHREW
-	db 15, SPEAROW
-	db 12, SANDSHREW
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
-	db 15, SANDSHREW
-ENDC
-	db 17, SPEAROW
-	db 11, DROWZEE
+	db 17, SPEAROW  ; 15.2
+	db 16, EKANS	; 29.7
+	db 16, SANDSHREW; 29.7
 	db 15, DROWZEE
+	db 18, PIDGEOTTO
+	db 20, PIDGEOTTO; 10.2
+	db 11, DROWZEE
+	db 22; DROWZEE  ; 15.3
 
 	db 0 ; water encounter rate
