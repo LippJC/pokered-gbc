6fbd1212b5fc9b620d06f6264980363301a1fce4
LippJC <lipp.jessec@gmail.com>
2022-08-03 20:04:40 -0400

    Update SeafoamIslandsB1F.asm

--- data/wild/maps/SeafoamIslandsB1F.asm
+++ data/wild/maps/SeafoamIslandsB1F.asm
@@ -1,28 +1,14 @@
 IslandMonsB1:
 	db 10 ; grass encounter rate
-IF DEF(_RED)
-	db 30, STARYU
-	db 30, HORSEA
-	db 32, SHELLDER
-	db 32, HORSEA
-	db 28, SLOWPOKE
-	db 30, SEEL
-	db 30, SLOWPOKE
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, SEADRA
-ENDC
-IF DEF(_BLUE)
-	db 30, SHELLDER
+	db 26, ZUBAT
 	db 30, KRABBY
-	db 32, STARYU
-	db 32, KRABBY
+	db 32, SLOWPOKE
+	db 32, SHELLDER
 	db 28, PSYDUCK
 	db 30, SEEL
-	db 30, PSYDUCK
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, KINGLER
-ENDC
+	db 30, SLOWPOKE
+	db 30, KINGLER
+	db 35, GOLBAT
+	db 37, SLOWBRO
 
 	db 0 ; water encounter rate
