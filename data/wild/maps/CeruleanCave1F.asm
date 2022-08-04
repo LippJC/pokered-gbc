4965ff5cfc0bc10493fa72306269a83ba4e7dbb1
LippJC <lipp.jessec@gmail.com>
2022-08-04 13:07:48 -0400

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,19 +1,14 @@
 DungeonMons1:
 	db 10 ; grass encounter rate
-	db 46, GOLBAT
-	db 46, HYPNO
-	db 46, MAGNETON
-	db 49, DODRIO
-	db 49, VENOMOTH
-IF DEF(_RED)
-	db 52, ARBOK
-ENDC
-IF DEF(_BLUE)
+	db 50, GOLBAT
+	db 55, HYPNO
+	db 45, MAGNETON
+	db 55, VENOMOTH
+	db 55, ARBOK
 	db 52, SANDSLASH
-ENDC
-	db 49, KADABRA
-	db 52, PARASECT
-	db 53, RAICHU
-	db 53, DITTO
+	db 54, KADABRA
+	db 54, PARASECT
+	db 55, RAICHU
+	db 60, DITTO
 
 	db 0 ; water encounter rate
