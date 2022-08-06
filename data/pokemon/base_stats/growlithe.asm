322cfee6ab3ac3dd56ca2a7bcbb52ef554fc9483
LippJC <lipp.jessec@gmail.com>
2022-08-05 20:39:20 -0400

    MOVE TYPE CHANGE + DMG CHANGE

--- data/pokemon/base_stats/growlithe.asm
+++ data/pokemon/base_stats/growlithe.asm
@@ -17,7 +17,7 @@
 	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  RAGE,         \
 	     DRAGON_RAGE,  DIG,          MIMIC,        DOUBLE_TEAM,  REFLECT,      \
 	     BIDE,         FIRE_BLAST,   SWIFT,        SKULL_BASH,   REST,         \
-	     SUBSTITUTE	   FLASH
+	     SUBSTITUTE,   FLASH
 	; end
 
 	db BANK(GrowlithePicFront)
