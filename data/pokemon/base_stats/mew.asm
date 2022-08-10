7c518feaa8612bf62fb25422b1e1f585ce25c110
LippJC <lipp.jessec@gmail.com>
2022-08-10 10:46:18 -0400

    Firepunch tm edit
    Fixed layout

--- data/pokemon/base_stats/mew.asm
+++ data/pokemon/base_stats/mew.asm
@@ -21,7 +21,7 @@
 	     MEGA_DRAIN,   SOLARBEAM,    DRAGON_RAGE,  THUNDERBOLT,  THUNDER,      \
 	     EARTHQUAKE,   FISSURE,      DIG,          PSYCHIC_M,    TELEPORT,     \
 	     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    \
-	     SELFDESTRUCT, FIRE_BLAST,   SWIFT,        SKULL_BASH,   MIRROR_MOVE,  \
+	     SELFDESTRUCT, MIRROR_MOVE,  FIRE_BLAST,   SWIFT,        SKULL_BASH,   \
 	     FIRE_PUNCH,   DREAM_EATER,  SKY_ATTACK,   REST,         THUNDER_WAVE, \
 	     PSYWAVE,      EXPLOSION,    ROCK_SLIDE,   TRI_ATTACK,   SUBSTITUTE,   \
 	     CUT,          FLY,          SURF,         STRENGTH,     FLASH,        \
